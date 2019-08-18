5fb6a195e9f6ba8e379e1606b876b560a74fa111
zhangyaxiong1991 <13201602191@163.com>
2019-08-18 13:31:27 +0800

    部署脚本

---
+++ deploy/merge_files.py
@@ -0,0 +1,46 @@
+# coding:utf-8
+import os
+import sys
+import locale
+from modulefinder import ModuleFinder
+
+root_dir = os.path.dirname(os.path.dirname(__file__))
+root_dir = root_dir.replace('/', os.sep)
+file_path = __file__.replace('/', os.sep)
+
+if __name__ == '__main__':
+    finder = ModuleFinder(encoding='utf-8')
+    finder.run_script(r'D:\code\MindgoIncrementalFramework\policys\test01.py')
+
+    file_list = []
+    keys = finder.modules.keys()
+    keys = reversed(keys)
+    for key in keys:
+        m = finder.modules[key]
+        if m.__path__:
+            print("P", end=' ')
+        else:
+            print("m", end=' ')
+        print("%-25s" % key, m.__file__ or "")
+
+    keys = finder.modules.keys()
+    keys = reversed(keys)
+    for key in keys:
+        m = finder.modules[key]
+        if m.__file__ is None:
+            continue
+        if m.__file__ == file_path:
+            continue
+        if not m.__file__.startswith(root_dir):
+            continue
+        file_list.append(m.__file__)
+
+    merge_file = ''
+    for file in file_list:
+        with open(file, encoding='utf-8') as f:
+            for line in f:
+                if line.startswith('import') or ' import ' in line:
+                    continue
+                merge_file += line
+    with open(os.sep.join([root_dir, 'output', 'ot.py']), 'w', encoding='utf-8') as f:
+        f.write(merge_file)
