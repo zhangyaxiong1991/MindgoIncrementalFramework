5cb8229f3d7e6dde87077a8b38a4baf4f2ea0666
zhangyaxiong1991 <13201602191@163.com>
2020-01-03 01:04:26 +0800

    融合趋势完成

--- normal_styles/box.py
+++ normal_styles/box.py
@@ -5,11 +5,11 @@ from normal_styles.trend import Trend
 
 
 class Box(BaseStyle):
-    column_name = 'k_box'
-    down_trend_start = 'k_box_down_start'
-    down_trend_end = 'k_box_down_end'
-    up_trend_start = 'k_box_up_start'
-    up_trend_end = 'k_box_up_end'
+    column_name = 'box'
+    down_trend_start = 'box_ds'
+    down_trend_end = 'box_de'
+    up_trend_start = 'box_us'
+    up_trend_end = 'box_ue'
     column_names = [column_name, down_trend_start, down_trend_end, up_trend_start, up_trend_end]
 
     上涨 = 1
@@ -83,32 +83,37 @@ class Box(BaseStyle):
 
                 now_status = Box.下跌
 
+        if now_status == Box.上涨:
+            log.info("end set up:", up_start, len(style_data.index)-1)
+            self._set_up_trend(style_data, up_start, len(style_data.index)-1)
+        if now_status == Box.下跌:
+            log.info("end set down:", down_start, len(style_data.index) - 1)
+            self._set_down_trend(style_data, down_start, len(style_data.index)-1)
+
 
 class MergedBox(BaseStyle):
     column_name = 'merged_box'
-    down_merge_start = 'merged_box_down_merge_start'
-    down_merge_end = 'merged_box_down_merge_end'
-    up_merge_start = 'merged_box_up_merge_start'
-    up_merge_end = 'merged_box_up_merge_end'
+    down_merge_start = 'merged_box_ds'
+    down_merge_end = 'merged_box_de'
+    up_merge_start = 'merged_box_us'
+    up_merge_end = 'merged_box_ue'
     column_names = [column_name, down_merge_start, down_merge_end, up_merge_start, up_merge_end]
 
     def _set_down_merge_trend(self, style_data, down_merge_start, down_merge_end):
         i = down_merge_start
-        while i < down_merge_end:
+        while i <= down_merge_end:
             style_data.iloc[i][MergedBox.down_merge_start] = down_merge_start
             style_data.iloc[i][MergedBox.down_merge_end] = down_merge_end
-            style_data.iloc[i][MergedBox.up_merge_start] = -1
-            style_data.iloc[i][MergedBox.up_merge_end] = -1
             i += 1
+        log.info(style_data.iloc[down_merge_start: down_merge_end])
 
     def _set_up_merge_trend(self, style_data, up_merge_start, up_merge_end):
         i = up_merge_start
-        while i < up_merge_end:
+        while i <= up_merge_end:
             style_data.iloc[i][MergedBox.up_merge_start] = up_merge_start
             style_data.iloc[i][MergedBox.up_merge_end] = up_merge_end
-            style_data.iloc[i][MergedBox.down_merge_start] = -1
-            style_data.iloc[i][MergedBox.down_merge_end] = -1
             i += 1
+        log.info(style_data.iloc[up_merge_start: up_merge_end])
 
     def _merge_down_trends_once(self, stock_data, style_data):
         now_down_trend = None
@@ -126,17 +131,13 @@ class MergedBox(BaseStyle):
             else:
                 # 结尾不想等表示遇到了新的趋势
                 if now_down_trend[1] < down_trend[1]:
-                    back_range = (stock_data.iloc[down_trend[0]]['high'] - stock_data.iloc[now_down_trend[1]][
-                        'low']) / (stock_data.iloc[now_down_trend[0]]['high'] - stock_data.iloc[now_down_trend[1]][
-                        'low'])
-                    out_range = (stock_data.iloc[now_down_trend[1]]['low'] - stock_data.iloc[down_trend[1]]['low']) / (
-                                stock_data.iloc[now_down_trend[0]]['high'] - stock_data.iloc[now_down_trend[1]]['low'])
-                    if out_range > back_range:
+                    if self.merge_down(stock_data, now_down_trend, down_trend):
                         now_down_trend[1] = down_trend[1]
                         times += 1
                     else:
                         self._set_down_merge_trend(style_data, now_down_trend[0], now_down_trend[1])
                         now_down_trend = down_trend
+        self._set_down_merge_trend(style_data, now_down_trend[0], now_down_trend[1])
         return times
 
     def _merge_up_trends_once(self, stock_data, style_data):
@@ -153,16 +154,13 @@ class MergedBox(BaseStyle):
             else:
                 # 结尾不想等表示遇到了新的趋势
                 if now_up_trend[1] < up_trend[1]:
-                    back_range = (stock_data.iloc[now_up_trend[1]]['high'] - stock_data.iloc[up_trend[0]]['low']) / (
-                                stock_data.iloc[now_up_trend[1]]['high'] - stock_data.iloc[now_up_trend[0]]['low'])
-                    out_range = (stock_data.iloc[up_trend[1]]['high'] - stock_data.iloc[now_up_trend[1]]['high']) / (
-                                stock_data.iloc[now_up_trend[1]]['high'] - stock_data.iloc[now_up_trend[0]]['low'])
-                    if out_range > back_range:
+                    if self.merge_up(stock_data, now_up_trend, up_trend):
                         now_up_trend[1] = up_trend[1]
                         times += 1
                     else:
                         self._set_up_merge_trend(style_data, now_up_trend[0], now_up_trend[1])
                         now_up_trend = up_trend
+        self._set_up_merge_trend(style_data, now_up_trend[0], now_up_trend[1])
         return times
 
     def _merge_down_trends(self, stock_data, style_data):
@@ -189,6 +187,39 @@ class MergedBox(BaseStyle):
             if times == 0:
                 break
 
+    def merge_up(self, stock_data, left_up_trend, right_up_trend):
+        back_range = (stock_data.iloc[left_up_trend[1]]['high'] - stock_data.iloc[right_up_trend[0]]['low']) / (
+                stock_data.iloc[left_up_trend[1]]['high'] - stock_data.iloc[left_up_trend[0]]['low'])
+        out_range = (stock_data.iloc[right_up_trend[1]]['high'] - stock_data.iloc[left_up_trend[1]]['high']) / (
+                stock_data.iloc[left_up_trend[1]]['high'] - stock_data.iloc[left_up_trend[0]]['low'])
+        if out_range > back_range:
+            log.info("merge up", left_up_trend, right_up_trend, out_range, back_range, stock_data.index[left_up_trend[0]])
+            return True
+        return False
+
+    def merge_down(self, stock_data, left_down_trend, right_down_trend):
+        back_range = (stock_data.iloc[right_down_trend[0]]['high'] - stock_data.iloc[left_down_trend[1]][
+            'low']) / (stock_data.iloc[left_down_trend[0]]['high'] - stock_data.iloc[left_down_trend[1]][
+            'low'])
+        out_range = (stock_data.iloc[left_down_trend[1]]['low'] - stock_data.iloc[right_down_trend[1]]['low']) / (
+                stock_data.iloc[left_down_trend[0]]['high'] - stock_data.iloc[left_down_trend[1]]['low'])
+        if out_range > back_range:
+            log.info("merge down", left_down_trend, right_down_trend, out_range, back_range)
+            return True
+        return False
+
+    def _large_eat_small(self, stock_data, style_data):
+        for i in range(len(style_data.index)):
+            if style_data.iloc[i][MergedBox.up_merge_start] > 0 and style_data.iloc[i][MergedBox.up_merge_end] > 0 and style_data.iloc[i][MergedBox.down_merge_start] > 0 and style_data.iloc[i][MergedBox.down_merge_end] > 0:
+                if style_data.iloc[i][MergedBox.up_merge_start] < style_data.iloc[i][MergedBox.down_merge_start] and style_data.iloc[i][MergedBox.up_merge_end] > style_data.iloc[i][MergedBox.down_merge_end]:
+                    style_data.iloc[i][MergedBox.down_merge_start] = -1
+                    style_data.iloc[i][MergedBox.down_merge_end] = -1
+
+                if style_data.iloc[i][MergedBox.up_merge_start] > style_data.iloc[i][MergedBox.down_merge_start] and style_data.iloc[i][MergedBox.up_merge_end] < style_data.iloc[i][MergedBox.down_merge_end]:
+                    style_data.iloc[i][MergedBox.up_merge_start] = -1
+                    style_data.iloc[i][MergedBox.up_merge_end] = -1
+
+
     def set_data(self, stock_data, style_data):
         self.add_column_data(style_data, MergedBox.down_merge_start, -1)
         self.add_column_data(style_data, MergedBox.down_merge_end, -1)
@@ -196,3 +227,4 @@ class MergedBox(BaseStyle):
         self.add_column_data(style_data, MergedBox.up_merge_end, -1)
         self._merge_down_trends(stock_data, style_data)
         self._merge_up_trends(stock_data, style_data)
+        self._large_eat_small(stock_data, style_data)
